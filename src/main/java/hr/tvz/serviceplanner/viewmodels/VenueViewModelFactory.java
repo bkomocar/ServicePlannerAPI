bf1843b9e563e1090c235589d919a751878132c5
Barbara Komočar <bkomocar@gmail.com>
2016-11-03 19:55:05 +0100

    Return venue open and close time in HH:mm format

--- src/main/java/hr/tvz/serviceplanner/viewmodels/VenueViewModelFactory.java
+++ src/main/java/hr/tvz/serviceplanner/viewmodels/VenueViewModelFactory.java
@@ -1,5 +1,7 @@
 package hr.tvz.serviceplanner.viewmodels;
 
+import java.sql.Time;
+import java.text.SimpleDateFormat;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -13,8 +15,9 @@ public class VenueViewModelFactory {
 		if (venue != null) {
 			if (type == null || type == ViewModelType.large || type == ViewModelType.medium
 					|| type == ViewModelType.extended) {
+				SimpleDateFormat sdf = new SimpleDateFormat("HH:mm");
 				return new VenueViewModelLarge(venue.getId(), venue.getName(), venue.getDescription(), venue.getOwner(),
-						venue.getOpenTime().toString(), venue.getCloseTime().toString(), venue.getType());
+						sdf.format(venue.getOpenTime()), sdf.format(venue.getCloseTime()), venue.getType());
 			} else {
 				return new VenueViewModelSmall(venue.getId(), venue.getName(), venue.getType());
 			}
