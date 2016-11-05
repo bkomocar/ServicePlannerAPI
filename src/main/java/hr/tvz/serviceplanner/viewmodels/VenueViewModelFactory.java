b844aee654181e5077dbfd2d4e2c40e7fc11fe2f
Barbara Komočar <bkomocar@gmail.com>
2016-11-05 20:39:40 +0100

    Create VenueViewModelExtended for sending the list of groups related to the venue

--- src/main/java/hr/tvz/serviceplanner/viewmodels/VenueViewModelFactory.java
+++ src/main/java/hr/tvz/serviceplanner/viewmodels/VenueViewModelFactory.java
@@ -2,10 +2,14 @@ package hr.tvz.serviceplanner.viewmodels;
 
 import java.sql.Time;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import hr.tvz.serviceplanner.persistence.models.Group;
+import hr.tvz.serviceplanner.persistence.models.Product;
 import hr.tvz.serviceplanner.persistence.models.Venue;
+import hr.tvz.serviceplanner.viewmodels.response.VenueViewModelExtended;
 import hr.tvz.serviceplanner.viewmodels.response.VenueViewModelLarge;
 import hr.tvz.serviceplanner.viewmodels.response.VenueViewModelSmall;
 
@@ -13,11 +17,16 @@ public class VenueViewModelFactory {
 
 	public static VenueViewModel toVenueViewModel(Venue venue, ViewModelType type) {
 		if (venue != null) {
-			if (type == null || type == ViewModelType.large || type == ViewModelType.medium
-					|| type == ViewModelType.extended) {
+
+			if (type == null || type == ViewModelType.large || type == ViewModelType.medium) {
 				SimpleDateFormat sdf = new SimpleDateFormat("HH:mm");
 				return new VenueViewModelLarge(venue.getId(), venue.getName(), venue.getDescription(), venue.getOwner(),
 						sdf.format(venue.getOpenTime()), sdf.format(venue.getCloseTime()), venue.getType());
+			} else if (type == ViewModelType.extended) {
+				SimpleDateFormat sdf = new SimpleDateFormat("HH:mm");
+				List<Group> groups = new ArrayList<>(venue.getGroups());
+				return new VenueViewModelExtended(venue.getId(), venue.getName(), venue.getDescription(), venue.getOwner(),
+						sdf.format(venue.getOpenTime()), sdf.format(venue.getCloseTime()), venue.getType(), GroupViewModelFactory.toGroupViewModel(groups, ViewModelType.large));
 			} else {
 				return new VenueViewModelSmall(venue.getId(), venue.getName(), venue.getType());
 			}
